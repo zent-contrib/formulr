1318f4b5c3e5e4bd2b03d7f030f57813c1775e4e
intellild <intellild@outlook.com>
2019-04-11 18:08:46 +0800

    refactor form with async validate

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export * from './field';
-export * from './field-array';
-export * from './field-set';
-export * from './form';
-export * from './models';
+// export * from './field';
+// export * from './field-array';
+// export * from './field-set';
+// export * from './form';
+// export * from './models';
 
-export { Validator, IVerifyOption } from './shared';
+// export { Validator, IVerifyOption } from './shared';
