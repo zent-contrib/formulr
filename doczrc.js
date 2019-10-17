9c47f731b52d807e38c706df8ec72d549176f30f
intellild <intellild@outlook.com>
2019-10-17 11:38:39 +0800

    update doc script

--- doczrc.js
+++
@@ -1,9 +0,0 @@
-export default {
-  src: './doc',
-  typescript: true,
-  // modifyBundlerConfig: (config) => {
-  //   config.resolve.extensions.push('.tsx', '.ts')
-  //   throw config;
-  //   return config
-  // }
-};
