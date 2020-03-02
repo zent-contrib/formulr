d628f33e46beae9ffa494876908a9c10cbff87b9
intellild <intellild@outlook.com>
2020-03-02 17:32:19 +0800

    add missing file

---
+++ src/models/base.ts
@@ -0,0 +1,22 @@
+import { IMaybeError, IValidators, ValidateOption } from '../validate';
+
+export interface IModel<Value> {
+  getRawValue(): any;
+  getSubmitValue(): any;
+  pristine(): boolean;
+  touched(): boolean;
+  dirty(): boolean;
+  valid(): boolean;
+  patchValue(value: Value): void;
+  validate(strategy: ValidateOption): Promise<any>;
+  reset(): void;
+  clear(): void;
+  initialize(value: Value): void;
+  error: IMaybeError<Value>;
+  owner: IModel<any> | null;
+  form: IForm<any> | null | undefined;
+  dispose(): void;
+  validators: IValidators<Value>;
+}
+
+export interface IForm<Value> extends IModel<Value> {}
