b16a59bda0d2e4996599b9c90d2a84c772a98f0d
wangjiwei <wangjiwei@youzan.com>
2020-04-23 12:47:31 +0800

    fix: dynamicMessage的参数函数接收校验上下文作为参数

--- src/validator-middlewares/dynamic-message.ts
+++ src/validator-middlewares/dynamic-message.ts
@@ -1,51 +1,32 @@
-import { UnknownObject } from '../utils';
 import { IValidator, isAsyncValidator, createAsyncValidator, IMaybeError, ValidatorContext } from '../validate';
-import { isObservable, of } from 'rxjs';
+import { of, from } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 
-function withMessage<F extends UnknownObject = UnknownObject, V = unknown>(
+function withMessage<V = unknown>(
   maybeError: IMaybeError<V>,
-  messagenerator: (formValue: F) => string,
-  formValue: F,
+  messagenerator: (ctx: ValidatorContext<V>) => string,
+  ctx: ValidatorContext<V>,
 ) {
-  return maybeError && { ...maybeError, message: messagenerator(formValue) };
+  return maybeError && { ...maybeError, message: messagenerator(ctx) };
 }
 
 /**
  * 为校验错误设定动态的message
  * @param messagenerator
  */
-export function dynamicMessage<F extends UnknownObject = UnknownObject, V = unknown>(
-  messagenerator: (formValue: F) => string,
-) {
+export function dynamicMessage<V = unknown>(messagenerator: (ctx: ValidatorContext<V>) => string) {
   return (validator: IValidator<V>) => {
     if (isAsyncValidator(validator)) {
       return createAsyncValidator<V>((value, context) => {
-        const formValue = context.getFormValue() as F;
-        if (!formValue) {
-          throw new Error('Validation is aborted due to context.getFormValue() returned null or undefined.');
-        }
         const result = validator.validator(value, context);
 
-        if (isObservable(result)) {
-          return result.pipe(
-            switchMap(maybeError => {
-              return of(withMessage(maybeError, messagenerator, formValue));
-            }),
-          );
-        } else if (result instanceof Promise) {
-          return result.then(maybeError => withMessage(maybeError, messagenerator, formValue));
-        } else {
-          return result;
-        }
+        return (
+          result && from(result).pipe(switchMap(maybeError => of(withMessage(maybeError, messagenerator, context))))
+        );
       });
     } else {
       return (value: V, context: ValidatorContext<V>) => {
-        const formValue = context.getFormValue() as F;
-        if (!formValue) {
-          throw new Error('Validation is aborted due to context.getFormValue() returned null or undefined.');
-        }
-        return withMessage(validator(value, context), messagenerator, formValue);
+        return withMessage(validator(value, context), messagenerator, context);
       };
     }
   };
