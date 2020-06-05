fd116f8f177718825e8583fe411390cb43350ca2
wangjiwei <wangjiwei@youzan.com>
2020-06-05 16:17:57 +0800

    chore: IWithLength放到文件开头

--- src/validators.ts
+++ src/validators.ts
@@ -2,6 +2,10 @@ import Decimal from 'big.js';
 import { ISyncValidator, IMaybeError, IValidator } from './validate';
 import { isNil } from './utils';
 
+export interface IWithLength {
+  length: number;
+}
+
 const EMAIL_REGEXP = /^(?=.{1,254}$)(?=.{1,64}@)[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+(\.[-!#$%&'*+/0-9=?A-Z^_`a-z{|}~]+)*@[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?(\.[A-Za-z0-9]([A-Za-z0-9-]{0,61}[A-Za-z0-9])?)*$/;
 
 function isEmptyInputValue(value: any) {
@@ -134,10 +138,6 @@ export function email(message?: string): ISyncValidator<string> {
         };
   }
   return email;
-}
-
-export interface IWithLength {
-  length: number;
 }
 
 /**
